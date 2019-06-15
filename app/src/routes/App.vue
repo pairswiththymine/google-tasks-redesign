c2a994f941aaaea657281014ee9513a0d3a96079
alexa <alexa.griffin42@gmail.com>
2019-06-14 22:42:37 -0400

    and finnally getting there

--- app/src/routes/App.vue
+++ app/src/routes/App.vue
@@ -1,23 +1,64 @@
 <template>
-  <Header />
+  <div>
+    <Header />
+    <aside>
+      <ul v-if="taskLists">
+        <li v-for="taskList in taskLists" v-bind:key="taskList.id" v-bind:class="taskLists.indexOf(taskList) === active ? 'active' : ''">{{ taskList.title }}</li>
+      </ul>
+    </aside>
+  </div>
 </template>
 
 <script>
 import Header from "../components/Header.vue"
 import api from "../api.js"
+
 export default {
   name: "app",
   components: {
     Header
   },
+  data: () => ({
+    taskLists: null,
+    active: 0
+  }),
   created() {
-    console.log(api.getTaskLists().then(console.log))
-    // console.log(api.authed())
     if(!api.authed()) this.$router.push("/")
+    else {
+      api.getTaskLists().then(res => {
+        this.taskLists = res.items
+        console.log(this)
+        console.log(res)
+      }, console.error)
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../_style.scss";
 
+aside {
+  height: 100vh;
+  width: 300px;
+  padding-top: 64px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  font-family: $main-font;
+  ul {
+    list-style: none;
+    padding-left: 0;
+    li {
+      border-top-right-radius: 5000px;
+      border-bottom-right-radius: 5000px;
+      padding: 8px;
+      margin: 8px 0;
+      cursor: pointer;
+      &.active, &:hover {
+        background-color: $alt-background;
+      }
+    }
+  }
+}
 </style>
