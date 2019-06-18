3f7d47a471d395ab2efe9f0c96cfe391861a3694
alexa <alexa.griffin42@gmail.com>
2019-06-18 04:42:11 -0400

    AAAAAAAAHHHHHHHHHHHHHHH

--- app/src/components/task-item.vue
+++ app/src/components/task-item.vue
@@ -1,14 +1,24 @@
 <template>
-  <div 
-    v-bind:class="'task ' + (expanded ? 'expanded' : 'not-expanded')"
-    v-on:click="expanded = true">
-    <div class="title">
+  <div v-bind:class="'task ' + (expanded ? 'expanded' : 'not-expanded')">
+    <div v-bind:class="'title-bar ' + (this.completed ? 'completed' : '')">
+      <button 
+        class="complete"
+        v-on:click="toggleComplete"
+      >
+        <img src="../assets/done.svg" alt="">
+      </button>
       <p class="title" v-show="!expanded">{{ task.title }}</p>
       <input 
         class="title" 
         v-show="expanded" 
         v-on:blur="saveNewNote"
         v-model="newTitle" />
+        <button class="expand">
+          <img 
+            v-on:click="expanded = !expanded"
+            v-bind:class="expanded ? 'up' : 'down'"
+            src="../assets/expand.svg">
+        </button>
     </div>
     <p v-if="task.due">{{ task.due }}</p>
     <p v-show="!expanded" class="notes">{{ shownNotes }}</p>
@@ -36,10 +46,12 @@ export default {
     expanded: false,
     newNotes: "",
     newTitle: "",
+    completed: false
   }),
   created() {
     this.newNotes = this.task.notes
     this.newTitle = this.task.title
+    this.completed = this.task.status === 'completed'
   },
   methods: {
     resizeArea(e) {
@@ -50,8 +62,13 @@ export default {
     saveNewNote() {
       api.updateTask(this.task.id, this.listId, {
         title: this.newTitle,
-        notes: this.newNotes
+        notes: this.newNotes,
+        status: this.completed ? "completed" : "needsAction"
       })
+    },
+    toggleComplete() {
+      this.completed = !this.completed
+      this.saveNewNote()
     }
   },
   watch: {
@@ -80,10 +97,55 @@ export default {
   &:hover {
     box-shadow: 0 1px 2px 0 rgba(60,64,67,0.302), 0 1px 3px 1px rgba(60,64,67,0.149);
   }
-  .title {
-    font-size: 1.5rem;
-    margin: 0 0 0.4rem;
-    padding: 0.2rem 0.3rem;
+  .complete {
+    height: 24px;
+    width: 24px;
+    border: 2px solid $alt-background;
+    background-color: transparent;
+    outline: none;
+    border-radius: 50%;
+    margin-right: 4px;
+    cursor: pointer;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    img {
+      vertical-align: middle;
+      position: relative;
+      top: -3px;
+      opacity: 0;
+    }
+  }
+  .completed>.complete>img {
+    opacity: 1;
+  }
+  .title-bar {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .title {
+      display: inline-block;
+      font-size: 1.5rem;
+      margin: 0 0 0.4rem;
+      padding: 0.2rem 0.3rem;
+      flex: 1;
+      min-width: 0;
+    }
+    .expand {
+      border: none;
+      outline: none;
+      background-color: transparent;
+      cursor: pointer;
+      img {
+        transition: 0.2s ease-in-out all;
+        &.down {
+          transform: rotate(0);
+        }
+        &.up {
+          transform: rotate(180deg)
+        }
+      }
+    }
   }
   input, textarea {
     background-color: $alt-background;
