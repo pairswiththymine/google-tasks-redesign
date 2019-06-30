3c831d5084bec700e5da3a11c5b9d86d2bf67838
alexa <alexa.griffin42@gmail.com>
2019-06-30 05:09:33 -0400

    these are actually really fun, i was kinda dreading this, but the design is really nice

--- app/src/components/date-picker.vue
+++ app/src/components/date-picker.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="date-picker">
     <div class="card">
-      <header>{{ new Date(0, activeMonth).toLocaleString('en-us', { month: 'long' }) }}</header>
+      <header>
+         <button></button>
+        <p>{{ new Date(activeYear, activeMonth).toLocaleString('en-us', { month: 'long' }) }}</p>
+        <button></button>
+      </header>
       <main>
-        <span v-for="(day, i) in days" v-bind:key="i">
-          {{ day.date }}
-        </span>
+        <div class="names">
+          <span>S</span>
+          <span>M</span>
+          <span>T</span>
+          <span>W</span>
+          <span>T</span>
+          <span>F</span>
+          <span>S</span>
+        </div>
+        <div class="dates">
+          <div v-bind:style="'width: calc(100% / 7 * ' + new Date(activeYear, activeMonth).getDay() + ')'"></div>
+          <span v-for="(day, i) in days" v-bind:key="i" class="date">
+            {{ day.date }}
+          </span>
+        </div>
       </main>
       <footer>
         <button>Cancel</button>
@@ -20,11 +36,12 @@
 export default {
   data: () => ({
     activeMonth: 1,
+    activeYear: 2019,
     days: []
   }),
 
-  watch: {
-    activeMonth() {
+  methods: {
+    createDays() {
       this.days = []
       const lastDay = new Date(2019, this.activeMonth + 1, 0)
       for(let i = 0; i < lastDay.getDate(); i++) {
@@ -33,6 +50,15 @@ export default {
         })
       }
     }
+  },
+
+  watch: {
+    activeMonth() {
+      this.createDays()
+    }
+  },
+  created() {
+    this.createDays()
   }
 }
 </script>
@@ -51,6 +77,11 @@ export default {
   left: 0;
   z-index: 1000000;
   font-family: $main-font;
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
   .overlay {
     height: 100%;
     width: 100%;
@@ -65,9 +96,10 @@ export default {
     border-radius: 8px;
     box-shadow: 0 12px 15px 0 rgba(0,0,0,0.24);
     width: 95%;
+    padding: 8px;
     max-width: 300px;
-    height: 200px;
     position: relative;
+    padding-bottom: 64px;
     footer {
       bottom: 0;
       left: 0;
@@ -75,6 +107,7 @@ export default {
       position: absolute;
       text-align: right;
       padding: 8px;
+      height: 64px;
       button {
         border-radius: 2px;
         background-color: transparent;
@@ -90,6 +123,29 @@ export default {
         &:last-child {
           color: $main;
         }
+      }
+    }
+    main {
+      div.dates {
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+      }
+      span {
+        font-size: 12px;
+        height: 32px;
+        width: calc(100% / 7);
+        margin-bottom: 4px;
+        text-align: center;
+        border-radius: 50%;
+        cursor: pointer;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+      }
+      .names>span {
+        color: $alt-color;
+        font-weight: 500;
       }
     }
   }
